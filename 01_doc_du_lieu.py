9814dbf919f53fca23989160114a8a89fc630804
HongHaiz <machhonghaipr@gmail.com>
2026-09-22 21:58:43 +0700

    clean data info output

--- 01_doc_du_lieu.py
+++ 01_doc_du_lieu.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-#Đọc dữ liệu
+# Đọc dữ liệu
 df = pd.read_csv("data/airline.csv")
 
 print("Kích thước dữ liệu:", df.shape)
 
-#Xem 5 dòng đầu
+# Xem 5 dòng đầu
 print(df.head())
 
-#Xem danh sách cột
+# Xem danh sách cột
 print("Danh sách cột:")
 for col in df.columns:
     print("-", col)
 
-#Xem thông tin dữ liệu
+# Xem thông tin dữ liệu
 print("Thông tin dữ liệu:")
-print(df.info())
+df.info()
